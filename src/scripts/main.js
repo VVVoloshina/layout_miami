4f7aa1c74c1dd359ec59c4ff90626f15eedcf0a1
Valeria0304 <lera_voloshina03@ukr.net>
2021-07-13 17:17:17 +0300

    fixed images and textarea

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,10 +3,14 @@
 const form = document.querySelector('.contact__form');
 const inputs = document.querySelectorAll('.contact__input');
 
+const textarea = document.querySelector('.contact__textarea');
+
 form.addEventListener('submit', (event) => {
   event.preventDefault();
 
   for (const input of inputs) {
     input.value = '';
   }
+
+  textarea.value = '';
 });
